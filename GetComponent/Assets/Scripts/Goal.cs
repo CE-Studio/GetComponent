8173ebf04adcd5bf1ed7f4e78efa408996ce2d02
EpsilonTheDerg <jamesclamey@gmail.com>
2021-06-11 23:33:02 -0400

    no message

---
+++ GetComponent/Assets/Scripts/Goal.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Goal : MonoBehaviour
+{
+    public AudioClip win;
+    
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            transform.GetChild(0).GetComponent<ParticleSystem>().Play();
+            GetComponent<AudioSource>().PlayOneShot(win);
+        }
+    }
+}
