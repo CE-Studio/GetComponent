d6f3a8553a610955cf20230f4be232d29e727ee3
EpsilonTheDerg <jamesclamey@gmail.com>
2021-06-12 23:58:12 -0400

    no message

--- GetComponent/Assets/Scripts/Levels.cs
+++ GetComponent/Assets/Scripts/Levels.cs
@@ -17,6 +17,7 @@ public class Levels
         camOrientations.Add(new Vector3(91, 1, 5));
         camOrientations.Add(new Vector3(112, 1, 5));
         camOrientations.Add(new Vector3(133, 1, 5));
+        camOrientations.Add(new Vector3(157.5f, 3, 7.25f));
 
         playerStartPos.Add(new Vector2(0.5f, 0.5f));
         playerStartPos.Add(new Vector2(23.5f, -1.5f));
@@ -25,6 +26,7 @@ public class Levels
         playerStartPos.Add(new Vector2(84.5f, -0.5f));
         playerStartPos.Add(new Vector2(105.5f, -1.5f));
         playerStartPos.Add(new Vector2(139.5f, 3.5f));
+        playerStartPos.Add(new Vector2(148.5f, 2.5f));
 
         playerPlugArrangements.Add("LRUD");
         playerPlugArrangements.Add("");
@@ -33,5 +35,6 @@ public class Levels
         playerPlugArrangements.Add("U");
         playerPlugArrangements.Add("UR");
         playerPlugArrangements.Add("R");
+        playerPlugArrangements.Add("LR");
     }
 }
